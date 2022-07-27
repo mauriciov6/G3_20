4ec6d2b89af8a96f5d1333487f474fc1294ee13f
mauriciov6 <carlos_vargas@unah.hn>
2022-07-27 13:01:57 -0600

    Correcion-models-libro

--- models/Libro.php
+++ models/Libro.php
@@ -7,8 +7,8 @@
             parent::set_names();
             $sql="SELECT * FROM libro ";
             $sql=$conectar->prepare($sql);
-            $sql->excute();
-            return $resultado=$sql->fetchAll(PDO::FECH_ASSOC);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
         }
 
         public function get_libro($codigo_de_libro){
@@ -16,9 +16,9 @@
             parent::set_names();
             $sql="SELECT * FROM libro where codigo_de_libro = ?";
             $sql=$conectar->prepare($sql);
-            $sql->binValue(1, $codigo_de_libro);            
-            $sql->excute();
-            return $resultado=$sql->fetchAll(PDO::FECH_ASSOC);
+            $sql->bindValue(1, $codigo_de_libro);            
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
         }
 
         public function insert_libro ($codigo_de_libro, $nombre_libro, $escritor_libro, $fecha_publicacion, $isbn, $precio, $editorial){
@@ -27,15 +27,15 @@
             $sql = "INSERT INTO libro(codigo_de_libro, nombre_libro, escritor_libro, fecha_publicacion, isbn, precio, editorial)
             VALUES (?,?,?,?,?,?,?);";
             $sql=$conectar->prepare($sql);
-            $sql->binValue(1, $codigo_de_libro);
-            $sql->binValue(2, $nombre_libro);
-            $sql->binValue(3, $escritor_libro);
-            $sql->binValue(4, $fecha_publicacion);
-            $sql->binValue(5, $isbn);
-            $sql->binValue(6, $precio);
-            $sql->binValue(7, $editorial);
-            $sql->excute();
-            return $resultado=$sql->fetchAll(PDO::FECH_ASSOC);            
+            $sql->bindValue(1, $codigo_de_libro);
+            $sql->bindValue(2, $nombre_libro);
+            $sql->bindValue(3, $escritor_libro);
+            $sql->bindValue(4, $fecha_publicacion);
+            $sql->bindValue(5, $isbn);
+            $sql->bindValue(6, $precio);
+            $sql->bindValue(7, $editorial);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);            
         }
 
         public function update_libro($codigo_de_libro, $nombre_libro, $escritor_libro, $fecha_publicacion, $isbn, $precio, $editorial){
@@ -44,15 +44,15 @@
             $sql = "UPDATE libro SET nombre_libro = ?, escritor_libro = ?, fecha_publicacion = ?, isbn = ?, precio = ?, editorial = ?
                     WHERE codigo_de_libro = ?;";
             $sql=$conectar->prepare($sql);
-            $sql->binValue(1, $codigo_de_libro);
-            $sql->binValue(2, $nombre_libro);
-            $sql->binValue(3, $escritor_libro);
-            $sql->binValue(4, $fecha_publicacion);
-            $sql->binValue(5, $isbn);
-            $sql->binValue(6, $precio);
-            $sql->binValue(7, $editorial);
-            $sql->excute();
-            return $resultado=$sql->fetchAll(PDO::FECH_ASSOC);
+            $sql->bindValue(1, $codigo_de_libro);
+            $sql->bindValue(2, $nombre_libro);
+            $sql->bindValue(3, $escritor_libro);
+            $sql->bindValue(4, $fecha_publicacion);
+            $sql->bindValue(5, $isbn);
+            $sql->bindValue(6, $precio);
+            $sql->bindValue(7, $editorial);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC); 
         }
 
         public function delete_libro($codigo_de_libro){
@@ -60,9 +60,9 @@
             parent::set_names();
             $sql="DELETE FROM libro where codigo_de_libro = ?";
             $sql=$conectar->prepare($sql);
-            $sql->binValue(1, $codigo_de_libro);            
-            $sql->excute();
-            return $resultado=$sql->fetchAll(PDO::FECH_ASSOC);            
+            $sql->bindValue(1, $codigo_de_libro);            
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);            
         }
     }
 ?>
